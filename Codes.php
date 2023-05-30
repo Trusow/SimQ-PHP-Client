fdefb3be959450f7cbd23ac56f0bf87dddd2a395
Трусов Сергей <tserg@hub.sknt.ru>
2023-05-30 19:16:34 +0300

    Codes: fish

---
+++ Codes.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace SimQ {
+    class Codes {
+        const CODE_OK = 10;
+        const CODE_ERR = 20;
+        const CODE_NO_SECURE = 102;
+        const CODE_GET_VERSION = 201;
+        const CODE_AUTH_GROUP = 1001;
+    }
+}
