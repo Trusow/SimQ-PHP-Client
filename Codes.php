bbb8d492504a1af5a356caea5afe9e5b80c71694
Трусов Сергей <tserg@hub.sknt.ru>
2023-06-01 19:07:06 +0300

    Codes: коды для consumers, producers

--- Codes.php
+++ Codes.php
@@ -6,11 +6,24 @@ namespace SimQ {
         const CODE_ERR = 20;
         const CODE_NO_SECURE = 102;
         const CODE_GET_VERSION = 201;
+
         const CODE_AUTH_GROUP = 1001;
+
         const CODE_GET_CHANNELS = 2001;
+        const CODE_GET_CONSUMERS = 2002;
+        const CODE_GET_PRODUCERS = 2003;
+
         const CODE_GET_CHANNEL_LIMIT_MESSAGES = 2101;
         const CODE_ADD_CHANNEL = 3001;
         const CODE_UPDATE_CHANNEL_LIMIT_MESSAGES = 3101;
         const CODE_REMOVE_CHANNEL = 3002;
+
+        const CODE_ADD_CONSUMER = 4001;
+        const CODE_REMOVE_CONSUMER = 4002;
+        const CODE_UPDATE_CONSUMER_PASSWORD = 4101;
+
+        const CODE_ADD_PRODUCER = 5001;
+        const CODE_REMOVE_PRODUCER = 5002;
+        const CODE_UPDATE_PROCUCER_PASSWORD = 5101;
     }
 }
