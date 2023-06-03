fadd16a011c733274820d6684570b92e34a4ed8d
Трусов Сергей <tserg@hub.sknt.ru>
2023-06-03 18:32:49 +0300

    Message: fish

---
+++ Message.php
@@ -0,0 +1,64 @@
+<?php 
+
+namespace SimQ {
+    require_once( './Codes.php' );
+
+    class Message {
+        private string $uuid = '';
+        private int $length = 0;
+        private int $offset = 0;
+        private $file = null;
+        private $data = '';
+
+        public function setLength( int $length ) {
+            $this->length = $length;
+        }
+
+        public function getLength() {
+            return $this->length;
+        }
+
+        public function isEmpty() {
+            return $this->length === 0;
+        }
+
+        public function setUUID( string $uuid ) {
+            $this->uuid = $uuid;
+        }
+
+        public function getUUID( string $uuid ) {
+            return $this->uuid;
+        }
+
+        public function isPublic( string $uuid ) {
+            return $this->uuid == '';
+        }
+
+        public function setPath( string $path ) {
+            $this->file = fopen( $path, 'w' );
+            fseek( $this->file, $offset );
+        }
+
+        public function setFile( $file, int $offset ) {
+            $this->file = $file;
+            fseek( $this->file, $offset );
+            $this->offset = $offset;
+        }
+
+        public function addData( string $data ) {
+            if( $this->file === null ) {
+                $this->data .= $data;
+                return;
+            }
+
+            $length = strlen( $data );
+            fwrite( $this->file, $data, $length );
+            $this->offset += $length;
+            fseek( $this->file, $this->offset );
+        }
+
+        public function getData() {
+            return $this->data;
+        }
+    };
+}
